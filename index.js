147a8110c5fe36c7cac2516d0f51b6f30582bd73
Henrique <henrique.dias@gruponewway.com.br>
2022-01-25 09:18:31 -0300

    feat: Using env PORT
    modified:   .gitignore
    modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
+app.listen(port, host, function () {
+  console.log(`Listening on: ${host}:${port}`)
 })
